d848f358d8d8bf88db23076328d93a46072035d0
jongGuk <qorwhdr@naver.com>
2023-05-21 21:49:53 +0900

    first commit

---
+++ src/test/java/com/batch/pass/BatchPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
