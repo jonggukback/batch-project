06edcec83a2efbc62ca78d75b74ceab91446ff9a
jongGuk <qorwhdr@naver.com>
2023-05-29 08:38:41 +0900

    pass 엔티티 레포지토리 생성

---
+++ src/main/java/com/batch/pass/repository/PassRepository.java
@@ -0,0 +1,18 @@
+package com.batch.pass.repository;
+
+import com.batch.pass.entity.pass.Pass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+
+public interface PassRepository extends JpaRepository<Pass, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Pass p" +
+            "          SET p.remainingCount = :remainingCount," +
+            "              p.modifiedAt = CURRENT_TIMESTAMP" +
+            "        WHERE p.passSeq = :passSeq")
+    int updateRemainingCount(Integer passSeq, Integer remainingCount);
+}
---
+++ src/test/java/com/batch/pass/config/TestBatchConfig.java
@@ -0,0 +1,2 @@
+package com.batch.pass.config;public class TestBatchConfig {
+}
